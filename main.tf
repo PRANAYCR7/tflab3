51dfabc1e25cb4c905c94ca863367949e03c4895
PRANAYCR7 <91401075+PRANAYCR7@users.noreply.github.com>
2024-11-07 19:14:27 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+#  resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+
+subscription_id      = "d5093934-1a30-426e-acb0-c14331a9b7fa"
+client_id            = "81316734-1a33-4d3a-95ff-4254d9f8af62"
+client_secret        = "aw18Q~JRQoyVHAWJgKNDS0bQEh2ENfCI.NU4XcGe"
+tenant_id            = "7974832e-4b9b-49e6-bc93-b5695f510220"
+}
+output "prefix_check" {
+   value = join("",["$var.prefix"],["RG01"])
+}
